48b062c18deb90aa61e4bc1338cb6b647dff27fe
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2024-09-16 00:50:08 +0530

    final code

--- add.cpp
+++ add.cpp
@@ -8,7 +8,7 @@ int main(void)
     int a, b, sum;
     cout << "\nEnter Two Numbers:\n";
     cin >> a >> b;
-    sum = a * b;
+    sum = a + b;
     cout << "\nThe sum is " << sum << endl;
     return 0;
 }
