1b8ff2e76961b9f461d4d42851e7b3fc747060a3
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-09-16 00:39:14 +0530

    performed addition

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    int a, b, sum;
+    cout << "\nEnter Two Numbers:\n";
+    cin >> a >> b;
+    sum = a * b;
+    cout << "\nThe sum is " << sum << endl;
+    return 0;
+}
